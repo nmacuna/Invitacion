8555dde50764500412325ee8262c9c3850ff93a0
nmacuna <106125117+nmacuna@users.noreply.github.com>
2025-02-17 17:11:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Muchas gracias, me encanta que digas que sí')
+    alert('Thanks, I don not have cookies :)')
 });
 
 const noBtn = document.querySelector('#noBtn');
